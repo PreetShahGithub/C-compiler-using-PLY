bcf0f7c63ebec1246c628256884d98d87037459d
PreetShahGithub <preethercules@gmail.com>
2019-03-28 19:34:32 +0530

    Add files via upload

---
+++ test.c
@@ -0,0 +1,28 @@
+# include < stdio.h >
+int main () {
+  int a = 5 , caa = 45.23 ,zzz , dddd = 56 , errrrrreeeeeeeerrrrrrrrrrrrrrrrrrrrrrrrrrrrrtttttttttttttttttttttteeeeerrrrrrrrrrrrrrrrrrrrrrrrrrrrrttttttttttttttttttttttffffr = 33, h = 66;
+  c = 20556;
+  int b = 39 + 5 / 2;
+    //b = 4 - 10;
+  b++;
+  b += 1;
+  do {
+    a = a + 5;
+      do {
+          b = b + 4;
+          if( b > 78){
+            printf("b");
+            if ( b < 90){
+              do{
+                dddd *= 2;
+              } while (dddd < 2000);
+              printf("awesome");
+            }
+            else{
+              printf("a");
+            }
+          }
+      } while ( a > b);
+  } while (a < b);
+  printf("hello world\n");
+}
